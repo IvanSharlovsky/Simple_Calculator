7ace971f30701632c35ee7c78aad005cb8e96497
Ivan <ivansharlovsky@gmail.com>
2023-03-29 03:35:11 +0300

    Made simple calculator

--- CalculatorTutorial.cpp
+++ CalculatorTutorial.cpp
@@ -2,13 +2,38 @@
 //
 
 #include <iostream>
+#include "Calculator.h"
 
 using namespace std;
 
 int main()
 {
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
     cout << "Calculator Console Application\n\n";
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+
+    Calculator c;
+    while (1)
+    {
+        cin >> x >> oper >> y;
+
+        if (oper == '/' && y == 0)
+        {
+            cout << "Attempted to divide by zero!\n";
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+
+        cout << "Result of " << x << oper << y << " is: " << result << endl;
+    }
+
     return 0;
 }
 
